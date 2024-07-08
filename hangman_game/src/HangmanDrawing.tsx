09b4eb2c5c7994e061996b1d412bbdb9e1277694
Martina Vangelova <mvangelovaa@abv.bg>
2024-07-08 11:04:28 +0300

    create parts of the body

--- hangman_game/src/HangmanDrawing.tsx
+++ hangman_game/src/HangmanDrawing.tsx
@@ -10,25 +10,75 @@ const HEAD = (
 
     }}/>
 )
+
 const BODY = (
     <div style ={{
         width: "10px",
         height: "100px",
-       
-        border: "10px sold black",
+      background:"black",
+        
         position: "absolute",
-        top:"50px",
-        right:"-25px",
+        top:"120px",
+        right:0,
 
     }}/>
 )
 
+const RIGHT_ARM = (
+   
+    <div style ={{
+        width: "100px",
+        height: "10px",
+      background:"black",
+        
+        position: "absolute",
+        top:"150px",
+        right: "-100px",
+ rotate:"-30deg",
+ transformOrigin:"left bottom",
+
+    }}/>
+)
+const LEFT_ARM = (
+   
+    <div style ={{
+        width: "100px",
+        height: "10px",
+      background:"black",
+        
+        position: "absolute",
+        top:"150px",
+        right: "10px",
+ rotate:"30deg",
+ transformOrigin:"right bottom",
+
+    }}/>
+)
+const RIGHT_LEG = (
+   
+    <div style ={{
+        width: "100px",
+        height: "10px",
+      background:"black",
+        
+        position: "absolute",
+        top:"150px",
+        right: "10px",
+ rotate:"60deg",
+ transformOrigin:"right bottom",
+
+    }}/>
+)
 export function HangmanDrawing(){
 
     return <div style= {{
         position:"relative" }}>
+            
             {HEAD}
             {BODY}
+            {RIGHT_ARM}
+            {LEFT_ARM}
+            {RIGHT_LEG}
             <div style={{
                 height: "50px",
                 width: "10px",
