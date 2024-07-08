246149c276818a47f981ad36ff4757ca513fc2c3
Martina Vangelova <mvangelovaa@abv.bg>
2024-07-08 11:23:36 +0300

    update hangman word component

--- hangman_game/src/HangmanWord.tsx
+++ hangman_game/src/HangmanWord.tsx
@@ -1,3 +1,17 @@
 export function HangmanWord(){
+const word = "test"
     
+    return <div style = 
+    {{display: "flex",
+     gap: ".25em",
+      fontSize:"6rem",
+      fontWeight: "bold",
+      textTransform: "uppercase",
+      fontFamily:"monospace",
+      
+      }}>
+{word.split("").map((letter,index) => (
+    <span style={{borderBottom: ".1em solid black"}}> {letter}</span>
+))}
+    </div>
 }
