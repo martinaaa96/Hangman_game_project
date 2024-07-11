3e79dd1dfc4536c962400d8dc18f2498b1787989
Martina Vangelova <mvangelovaa@abv.bg>
2024-07-11 13:06:31 +0300

    declareted type HandmanWord

--- hangman_game/src/HangmanWord.tsx
+++ hangman_game/src/HangmanWord.tsx
@@ -1,6 +1,10 @@
-export function HangmanWord(){
-const word = "test"
-const guessedLetters = ["e"]   
+type HangmanWordProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+}
+
+
+export function HangmanWord({guessedLetters, wordToGuess}:HangmanWordProps){
 
     return <div style = 
     {{display: "flex",
@@ -11,7 +15,7 @@ const guessedLetters = ["e"]
       fontFamily:"monospace",
       
       }}>
-{word.split("").map((letter,index) => (
+{wordToGuess.split("").map((letter,index) => (
     <span style={{borderBottom: ".1em solid black"}} key={index}>
         <span style= {{
             visibility:guessedLetters.includes(letter)?
