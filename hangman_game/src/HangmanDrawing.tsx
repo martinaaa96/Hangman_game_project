d1d7602b8ec581aaabc36eddab7be82a3549b2f2
Martina Vangelova <mvangelovaa@abv.bg>
2024-07-11 12:26:32 +0300

    Update HangmanDrawing component

--- hangman_game/src/HangmanDrawing.tsx
+++ hangman_game/src/HangmanDrawing.tsx
@@ -1,15 +1,16 @@
 const HEAD = (
-    <div style ={{
+    <div
+      style={{
         width: "50px",
         height: "50px",
         borderRadius: "100%",
-        border: "10px sold black",
+        border: "10px solid black",
         position: "absolute",
-        top:"50px",
-        right:"-25px",
-
-    }}/>
-)
+        top: "50px",
+        right: "-30px",
+      }}
+    />
+  )
 
 const BODY = (
     <div style ={{
@@ -85,6 +86,8 @@ const LEFT_LEG = (
     }}/>
 )
 
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM,LEFT_ARM, RIGHT_LEG,LEFT_LEG]
+
 type HangmanDrawingProps = {
     numberOfGuesses: number
 }
@@ -93,13 +96,7 @@ export function HangmanDrawing( {numberOfGuesses}: HangmanDrawingProps){
 
     return <div style= {{
         position:"relative" }}>
-            
-            {HEAD}
-            {BODY}
-            {RIGHT_ARM}
-            {LEFT_ARM}
-            {RIGHT_LEG}
-            {LEFT_LEG}
+          {BODY_PARTS}
             <div style={{
                 height: "50px",
                 width: "10px",
